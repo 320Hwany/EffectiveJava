ba801ebd6f944b377f95ab84b05b62bd926a1d3e
Jung-YouHwan <yhwjd99@gmail.com>
2023-07-03 16:25:39 +0900

    아이템 27 예제 코드

---
+++ src/main/java/effective/chapter5/item27/CustomArrayList.java
@@ -0,0 +1,29 @@
+package effective.chapter5.item27;
+
+import java.util.Arrays;
+
+public class CustomArrayList {
+
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+
+    transient Object[] elementData;
+
+    private int size;
+
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size) {
+            @SuppressWarnings("unchecked")
+            T[] result = (T[]) Arrays.copyOf(elementData, size, a.getClass());
+            return result;
+        }
+        System.arraycopy(elementData, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+}
